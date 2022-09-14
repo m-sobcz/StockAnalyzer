4da4ab9f66c83a90efe6b3d07253a8c5efab2f4c
MSobczak <m.sobcz@gmail.com>
2022-09-14 10:54:35 +0200

    Removing warnings - WebClient -> HttpClient

--- StockAnalyzer.Infrastructure/Scrape/HtmlSource/HtmlDownloader.cs
+++ StockAnalyzer.Infrastructure/Scrape/HtmlSource/HtmlDownloader.cs
@@ -2,6 +2,8 @@
 using StockAnalyzer.Infrastructure.Scrape.RawDataSource;
 using System;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace StockAnalyzer.Infrastructure.Scrape.HtmlSource
 {
@@ -15,9 +17,9 @@ namespace StockAnalyzer.Infrastructure.Scrape.HtmlSource
         public string GetHtml(string adressSuffix="")
         {
             if (BaseAdress is null) throw new InvalidOperationException("Adress is null!");
-            using WebClient client = new WebClient();
+            using HttpClient client = new();
             string adress = BaseAdress + adressSuffix;
-            string html = client.DownloadString(adress);
+            string html = client.GetStringAsync(adress).Result;
             return html;
         }
 
